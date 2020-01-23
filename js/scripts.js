fabb38e59159c08d96bc0af5580c59ae9516dc81
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-01-22 23:54:57 -0800

    refactor business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,10 @@
 var leapYear = function(year){
-  if(year % 100 === 0){
-    return false;
-  }else if (year % 4 === 0){
+  if ((year % 4 === 0) && (year % 100 !== 0)){
     return true;
   }
-  return false;
+  else {return false;
 };
-
+};
 
 
 
